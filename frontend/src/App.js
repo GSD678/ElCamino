cab1e4c665f69cb506c4fb364c229ff9180df892
Karim <kimo_678@live.com>
2022-09-22 16:26:24 -0400

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import {Container} from "react-bootstrap";
 
-
-function App() {
-  return (
-    <>
-      <h1>Welcome to El Camino</h1>
-    </>
-  );
+const App = () => {
+    return (
+        <div>
+            <h1>dsdasdasdasd asde d</h1>
+            <Header />
+            <main>
+                <h1>dsidsd</h1>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;

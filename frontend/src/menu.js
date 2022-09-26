62acaa1e48df492c13d8079dc1facb6cd3764533
Galilea Garcia <ggarcia87@ivytech.edu>
2022-09-26 18:44:18 -0400

    Dummy Menu -G

--- frontend/src/menu.js
+++ frontend/src/menu.js
@@ -2,10 +2,37 @@ const menu = [
     {
        id:1,
        name: "taco",
-       description:"test"
+       cost: "$2.00",
+       description: "test",
+       img: ""
     },
     {
         id:2,
+        name: "Tortas",
+        cost: "$1.00",
+        description: "test 2",
+        img: ""
+    },
+    {
+        id:3,
+        name: "Sopes",
+        cost: "$3.00",
+        description: "test 3",
+        img: ""
+    },
+    {
+        id:4,
+        name: "Pupusas",
+        cost: "$4.00",
+        description: "test 4",
+        img: ""
+    },
+    {
+        id:5,
+        name: "Soups",
+        cost: "$5.00",
+        description: "test 5",
+        img: ""
     }
 
 ]
